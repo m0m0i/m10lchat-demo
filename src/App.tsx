4a6832de2f3cce2f57362edea65414b2f32c6823
m0m0i <hirocorp01@gmail.com>
2021-11-27 09:09:43 +0900

    Create Chat UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { SignIn } from './SignIn';
+import React from "react";
+import { SignIn } from "./SignIn";
+import { Chat } from "./Chat";
 
 function App() {
-  return (
-    <SignIn />
-  );
+  // TODO: Clean up after implementing FB Auth stuff
+  const user = true;
+
+  return user ? <Chat /> : <SignIn />;
 }
 
 export default App;
