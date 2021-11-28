2270343d08757a0a7954e9da0a064426bf91c02d
m0m0i <hirocorp01@gmail.com>
2021-11-29 07:22:49 +0900

    Create AuthProvider

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,25 +1,27 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { User } from 'firebase/auth';
-import { auth } from '../service/firebase';
+import React, { createContext, useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { auth } from "../service/firebase";
 
 type AuthContextProps = {
-  currentUser: User | null | undefined
-}
+  currentUser: User | null | undefined;
+};
 
 const AuthContext = createContext<AuthContextProps>({ currentUser: undefined });
 
 const AuthProvider: React.FC = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<User | null | undefined>(undefined);
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+    undefined
+  );
 
   useEffect(() => {
-    auth.onAuthStateChanged(user => setCurrentUser(user));
+    auth.onAuthStateChanged((user) => setCurrentUser(user));
   }, []);
 
-  return(
+  return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export { AuthContext, AuthProvider };
