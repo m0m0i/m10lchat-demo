db8edf882590b30f28c565554b6a3b11ef1f6846
m0m0i <hirocorp01@gmail.com>
2021-11-29 09:29:23 +0900

    implement AuthProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "./auth/AuthProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
