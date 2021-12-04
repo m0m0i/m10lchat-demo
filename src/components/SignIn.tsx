03ba90cb532cfe4a3677304ed4f7688d0788ce3d
m0m0i <hirocorp01@gmail.com>
2021-12-04 13:51:22 +0900

    Update UI

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -18,31 +18,39 @@ import { signInWithGoogle } from "../service/firebase";
 const _signInWithGoogle = () => signInWithGoogle();
 
 export const SignIn = () => (
-  <Box maxW="md" mx="auto" mt="200">
-    <Heading textAlign="center" size="xl" fontWeight="extrabold">
-      Sign in to your account
-    </Heading>
-    <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
-      <Text as="span">Don&apos;t have an Google account?</Text>
-      <Link href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp">
-        You can create it from here
-      </Link>
-    </Text>
-    <Card>
-      <SimpleGrid colums={1} spacing="4">
-        <Text as="span" align="center">
-          Sign in with your Google account.
-        </Text>
-        <Button
-          color="currentColor"
-          variant="outline"
-          onClick={_signInWithGoogle}
-        >
-          <VisuallyHidden>Login with Google</VisuallyHidden>
-          <FaGoogle />
-        </Button>
-      </SimpleGrid>
-    </Card>
+  <Box
+    bg={useColorModeValue("gray.50", "inherit")}
+    minH="100vh"
+    py="12"
+    px={{ base: "4", lg: "8" }}
+    h="full"
+  >
+    <Box maxW="md" mx="auto" mt="200">
+      <Heading textAlign="center" size="xl" fontWeight="extrabold">
+        Sign in to your account
+      </Heading>
+      <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
+        <Text as="span">Don&apos;t have an Google account?</Text>
+        <Link href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp">
+          You can create it from here
+        </Link>
+      </Text>
+      <Card>
+        <SimpleGrid colums={1} spacing="4">
+          <Text as="span" align="center">
+            Sign in with your Google account.
+          </Text>
+          <Button
+            color="currentColor"
+            variant="outline"
+            onClick={_signInWithGoogle}
+          >
+            <VisuallyHidden>Login with Google</VisuallyHidden>
+            <FaGoogle />
+          </Button>
+        </SimpleGrid>
+      </Card>
+    </Box>
   </Box>
 );
 
