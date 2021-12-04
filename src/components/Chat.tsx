c02b22e6a3c3a729a8fd5309c6973713e3a29b88
m0m0i <hirocorp01@gmail.com>
2021-12-04 13:52:23 +0900

    done with firestore integration

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,13 +1,11 @@
-import { CheckCircleIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
-  Center,
   Flex,
   Input,
-  Tag,
+  InputGroup,
+  InputRightElement,
   Text,
-  useColorModeValue,
 } from "@chakra-ui/react";
 import {
   DocumentData,
@@ -17,27 +15,23 @@ import {
   query,
   serverTimestamp,
 } from "firebase/firestore";
-import moment from "moment";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import ScrollableFeed from "react-scrollable-feed";
 
 import { useAuthContext } from "../auth/AuthProvider";
+import { Message, MessageProps } from "../components/Message";
 import { db, signOut } from "../service/firebase";
 import { useFirestoreQuery } from "../service/hooks";
-import misc from "./styles/misc.module.css";
+import misc from "../styles/misc.module.css";
 
 const _signOut = () => signOut();
 
 export const Chat: React.FC = () => {
   const { currentUser } = useAuthContext();
 
-  if (currentUser !== undefined && currentUser !== null) {
-    const { uid, displayName, photoURL } = currentUser;
-  }
   const [newMessage, setNewMessage] = useState<string>("");
 
   const inputRef = useRef<HTMLInputElement>(null);
-  const bottomListRef = useRef<HTMLDivElement>(null);
 
   const messagesRef = collection(db, "messages");
   const messages = useFirestoreQuery(query(messagesRef, orderBy("createdAt")));
@@ -49,7 +43,6 @@ export const Chat: React.FC = () => {
   }, [inputRef]);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     setNewMessage(e.target.value);
   };
 
@@ -57,6 +50,7 @@ export const Chat: React.FC = () => {
     e.preventDefault();
 
     const trimmedMessage = newMessage.trim();
+
     if (trimmedMessage) {
       // Add new message in Firestore
       await addDoc(messagesRef, {
@@ -66,180 +60,93 @@ export const Chat: React.FC = () => {
         displayName: currentUser?.displayName,
         photoURL: currentUser?.photoURL,
       });
+
       // Clear input field
       setNewMessage("");
-      // Scroll down to the bottom of the list
-      bottomListRef.current?.scrollIntoView({ behavior: "smooth" });
     }
   };
 
-  const me = currentUser?.displayName;
-
   return (
-    <Center>
-      <Flex
-        w="100%"
-        maxW={1200}
-        height={800}
-        maxHeight="calc(100vh - 2rem)"
-        flexDirection="column"
-        px={{ base: "4", lg: "8" }}
-      >
-        <Flex justifyContent="space-between">
-          <Text fontSize="2rem" fontWeight="bold">
-            Multilingual Chat x Antidote demo
-          </Text>
-          <Button
-            ml={24}
-            color="currentColor"
-            variant="ghost"
-            onClick={_signOut}
-          >
-            Sign Out
-          </Button>
-        </Flex>
-        <Box
-          maxHeight="100%"
-          height="100%"
-          mt="50"
-          bg={useColorModeValue("gray.50", "inherit")}
-          pb={20}
-          borderRadius={12}
-          overflow="hidden"
-          boxShadow="0 2px 2px #0f0f0f04"
+    <Flex flexDirection="column" height="full">
+      <Box overflow="auto" height="full">
+        <Flex
+          flexDirection="column"
+          height="100vh"
+          py="4"
+          maxW="1024"
+          mx="auto"
         >
-          <MessageFeed
-            me={true}
-            unsentMessages={messages}
-            messages={messages}
-          />
-          <Box ref={bottomListRef} />
-          <Flex px={4} height={20} alignItems="center">
-            <Input
-              placeholder="Shoot a message"
-              size="lg"
-              bg="white"
-              variant="ghost"
-              _focus={{ outline: "none" }}
-              ref={inputRef}
-              type="text"
-              value={newMessage}
-              onChange={handleOnChange}
-              onKeyPress={(e) => {
-                if (e.key === "Enter") {
-                  handleOnSubmit(e);
-                }
-              }}
-            />
-          </Flex>
-        </Box>
-      </Flex>
-    </Center>
+          <Box border="2" borderColor="gray.200" py={8} mb={4} flex="1">
+            <Flex justifyContent="space-between">
+              <Text fontSize="2rem" fontWeight="bold">
+                Multilingual Chat x Antidote demo
+              </Text>
+              <Button
+                ml={24}
+                color="currentColor"
+                variant="ghost"
+                onClick={_signOut}
+              >
+                Sign Out
+              </Button>
+            </Flex>
+          </Box>
+          <MessageFeed messages={messages} />
+          <Box mb="6" mx="4">
+            <InputGroup mx="auto">
+              <Input
+                placeholder="Shoot a message"
+                pr="4.5rem"
+                bg="transparent"
+                variant="ghost"
+                _focus={{ outline: "none" }}
+                ref={inputRef}
+                type="text"
+                value={newMessage}
+                onChange={handleOnChange}
+                flex="1"
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    handleOnSubmit(e);
+                  }
+                }}
+              />
+              <InputRightElement width="4.5rem">
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  variant="ghost"
+                  onClick={handleOnSubmit}
+                  _focus={{ outline: "none" }}
+                >
+                  {"FIRE"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
-export type Message = {
-  __typename?: "Message";
-  id: string;
-  text: string;
-  sender: string;
-  createdAt: string;
-  updatedAt: string;
+export const MessageFeed: React.FC<DocumentData> = ({ messages }) => {
+  const scrollBottomRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    scrollBottomRef?.current?.scrollIntoView();
+  }, []);
+
+  return (
+    <ScrollableFeed className={misc.hideScroll}>
+      {messages
+        ?.sort((a: MessageProps, b: MessageProps) =>
+          a?.createdAt?.seconds <= b?.createdAt?.seconds ? -1 : 1
+        )
+        ?.map((message: MessageProps, key: number) => (
+          <Message {...message} key={key} />
+        ))}
+      <div ref={scrollBottomRef} />
+    </ScrollableFeed>
+  );
 };
-
-interface MessageFeedProps {
-  messages: DocumentData[];
-  unsentMessages: DocumentData[];
-  me: Boolean;
-}
-
-export const MessageFeed: React.FC<MessageFeedProps> = ({
-  unsentMessages,
-  me,
-  messages,
-}) => (
-  <ScrollableFeed className={misc.hideScroll}>
-    <Box p={6}>
-      {messages?.map((message: any, key: number) => (
-        <MessageBox message={message} isMine={me} isUnsent={false} key={key} />
-      ))}
-      {unsentMessages?.map((message: any, key: number) => (
-        <MessageBox
-          message={message}
-          isMine={false}
-          isUnsent={true}
-          key={key}
-        />
-      ))}
-    </Box>
-  </ScrollableFeed>
-);
-
-interface MessageProps {
-  message: Message;
-
-  isMine: Boolean;
-  isUnsent: Boolean;
-}
-
-export const MessageBox: React.FC<MessageProps> = ({
-  message,
-  isUnsent,
-  isMine,
-}) => (
-  <Flex my={2} p={2}>
-    <Flex flexDirection="column" width="100%">
-      <Tag
-        variant="subtle"
-        mb={2}
-        bg={isMine ? "_purple" : "gray.500"}
-        color="white"
-        ml={isMine ? "auto" : undefined}
-        mr={isMine ? undefined : "auto"}
-      >
-        {message?.sender}
-      </Tag>
-      <Flex
-        bg="gray.50"
-        pr={2}
-        py={2}
-        pl={4}
-        borderRadius={12}
-        boxShadow="0 2px 2px #0f0f0f0f"
-        ml={isMine ? "auto" : undefined}
-        mr={isMine ? undefined : "auto"}
-      >
-        <Text fontSize={15} maxWidth={400}>
-          {message?.text}
-        </Text>
-        <Flex
-          ml="auto"
-          mt="auto"
-          pl={4}
-          alignItems="center"
-          justifyContent="flex-end"
-        >
-          {isUnsent ? (
-            <>
-              <Text fontSize={12} color="gray.500">
-                {moment().format("hh:mm A")}
-              </Text>
-              {isMine ? (
-                <CheckCircleIcon fontSize={12} ml={2} color="gray.400" />
-              ) : null}
-            </>
-          ) : (
-            <>
-              <Text fontSize={12} color="gray.500">
-                {moment(message?.createdAt).format("hh:mm A")}
-              </Text>
-              {isMine ? (
-                <CheckCircleIcon fontSize={12} ml={2} color="green.400" />
-              ) : null}
-            </>
-          )}
-        </Flex>
-      </Flex>
-    </Flex>
-  </Flex>
-);
