7a7f216d99f52127fbe30236836bc25b5e38a13c
m0m0i <hirocorp01@gmail.com>
2021-12-09 07:46:57 +0900

    Update and add create-room feature

--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -1,39 +1,63 @@
-import { Box, Heading, List, ListItem } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  List,
+} from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
 
+import { roomCreateHandler } from "../service/beApiHandler";
 import { RoomListProps } from "../types";
 import { Room } from "../types";
 import { Card } from "./Card";
 import { RoomListItem } from "./RoomListItem";
-import { RoomSelector } from "./RoomSelector";
 
 export const RoomList: React.FC<RoomListProps> = ({
-  currentRoom,
+  setRoomInfo,
   onRoomSelect,
 }) => {
-  const groups: Room[] = [
-    {
-      id: "41e98a97-47e4-4e56-a114-9b42716633ea",
-      name: "Hey I am actually a room",
-      messageNum: 0,
-      users: [],
-      empty: true,
-    },
-    {
-      id: "fcad1dd1-9002-4793-9c4c-6a04d8999a5b",
-      name: "Hey I am a room",
-      messageNum: 0,
-      users: [],
-      empty: true,
-    },
-    {
-      id: "1a4bc5e1-6a89-4f63-9290-78f975cfbc3b",
-      name: "Hey I am also a room",
-      messageNum: 0,
-      users: [],
-      empty: true,
-    },
-  ];
+  const [groups, setGroups] = useState<Room[]>([]);
+  const [newRoom, setNewRoom] = useState<string>("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const url = `http://${process.env.REACT_APP_SERVER_ADDRESS}/chats/groups`;
+
+  // TODO: Create Loading...
+
+  useEffect(() => {
+    const getGroups = async () => {
+      try {
+        await fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setGroups(data);
+            setRoomInfo(data);
+          });
+      } catch (e) {
+        console.log((e as Error).message);
+      }
+    };
+    getGroups();
+  }, []);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewRoom(e.target.value);
+  };
+
+  const handleOnSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setNewRoom("");
+    const trimmedRoomName = newRoom.trim();
+    if (trimmedRoomName) {
+      const createdRoom = await roomCreateHandler(url, trimmedRoomName);
+      onRoomSelect(createdRoom.name);
+      setRoomInfo([createdRoom]);
+    }
+  };
 
   return (
     <Box maxW="xl" mx="auto" mt="200">
@@ -45,13 +69,41 @@ export const RoomList: React.FC<RoomListProps> = ({
           {groups
             ?.sort((a: Room, b: Room) => a.messageNum - b.messageNum)
             ?.map((room: Room, key: number) => (
-              <RoomListItem
-                {...room}
-                key={key}
-                handleSelectRoom={onRoomSelect}
-              />
+              <RoomListItem room={room} onRoomSelect={onRoomSelect} key={key} />
             ))}
         </List>
+        <Box mx="4" mt="4">
+          <InputGroup mx="auto">
+            <Input
+              placeholder="Or create new room?"
+              pr="4.5rem"
+              bg="transparent"
+              variant="ghost"
+              _focus={{ outline: "none" }}
+              ref={inputRef}
+              type="text"
+              value={newRoom}
+              onChange={handleOnChange}
+              flex="1"
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleOnSubmit(e);
+                }
+              }}
+            />
+            <InputRightElement width="4.5rem">
+              <Button
+                h="1.75rem"
+                size="sm"
+                variant="ghost"
+                onClick={handleOnSubmit}
+                _focus={{ outline: "none" }}
+              >
+                {"GO"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Box>
       </Card>
     </Box>
   );
