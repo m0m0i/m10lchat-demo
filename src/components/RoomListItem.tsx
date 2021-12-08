c259d962ae90d24a0d79f490b64719ad7800332a
m0m0i <hirocorp01@gmail.com>
2021-12-09 07:48:50 +0900

    Update Avatar props and clean up

--- src/components/RoomListItem.tsx
+++ src/components/RoomListItem.tsx
@@ -3,23 +3,19 @@ import React from "react";
 
 import { RoomListItemProps } from "../types";
 
-export const RoomListItem: React.FC<RoomListItemProps> = ({
-  id,
-  name,
-  users,
-  messageNum,
-  key,
-  empty,
-  handleSelectRoom,
-}) => {
+export const RoomListItem: React.FC<RoomListItemProps> = (
+  { room, onRoomSelect },
+  key
+) => {
+  const { id, name, users, messageNum } = room;
   if (!id) <p>{`There's no room yet`}</p>;
 
   return (
     <Flex rounded="md" align="flex-end">
-      <Avatar name={name} src="https://bit.ly/broken-link" size="xs" />
+      <Avatar name={name} src="" size="xs" getInitials={(n: string) => n[0]} />
       <Text
         as="button"
-        onClick={() => handleSelectRoom(name)}
+        onClick={() => onRoomSelect(name)}
         mx="4"
         fontWeight="bold"
         fontSize="lg"
