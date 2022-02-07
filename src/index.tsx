1678d3027f3ec6d256c0ba14327f9a20b9136edf
m0m0i <hirocorp01@gmail.com>
2022-02-08 07:20:42 +0900

    delete web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,6 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -32,8 +31,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
